dfcd44e8cf16b0c453675d2c70bdc1edb69b8e7d
Hironobu Nishikokura <nishiko@mindia.jp>
2015-11-28 15:12:21 +0700

    sqliteではなくmysqlを使うようにしてseed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+card = Card.create!(id: 1, content: '西小倉宏信のホームページ')
+
+
+card1 = Card.create!(content: '245cloudについて', parent_id: card.id)
+
+
+card1 = Card.create!(content: 'Ingressについて', parent_id: card.id)
+card1 = Card.create!(content: 'MUFGカプセルの運用', parent_id: card1.id)
+
+
+
+card1 = Card.create!(content: 'ラフノート株式会社について', parent_id: card.id)
+
+
+card1 = Card.create!(content: 'マカロニデイズについて', parent_id: card.id)
+
+
