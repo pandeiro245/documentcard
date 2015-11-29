f330c17b7c64d57874ec2df40403035bf93fbebc
Hironobu Nishikokura <nishiko@mindia.jp>
2015-11-29 14:46:21 +0900

    ファビコンと合宿ページヘの導線を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def link_card card
     if m = card.content.match(/^https:\/\/gyazo.com\/([0-9a-z]*)/)
       link_to (image_tag "https://i.gyazo.com/#{m[1]}.png"), card.content, {target: '_blank'}
-    elsif card.content.match(/^http/)
+    elsif card.content.match(/^http:\/\//)
       link_to card.content, card.content, target: '_blank'
     elsif match = card.content.match(/^card:([0-9]*)/)
       target_card = Card.find(match[1])
